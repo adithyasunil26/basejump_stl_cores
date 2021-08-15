b7dcc2319866f645bc3cd8ce492cc30ff8ca8dfc
adithyasunil26 <adithyasunil26@gmail.com>
2021-08-15 22:47:44 +0400

    Added scripts for testing all cores

---
+++ test-run/tb-test.py
@@ -0,0 +1,22 @@
+import os
+
+stl_cores_dir="../"
+
+os.system("fusesoc library add basejump "+stl_cores_dir)
+
+all_files=os.listdir(stl_cores_dir)
+core_directories=[]
+for i in all_files:
+  if i.startswith('bsg'):
+    core_directories.append(i)
+
+for i in core_directories:
+  cores_in_dir=os.listdir(stl_cores_dir+i)
+  for j in cores_in_dir:
+    j=j.split('.')[0]
+    print("fusesoc run --target verilator_tb "+j)
+    os.system("fusesoc run --target verilator_tb "+j)
+
+f.close()
+os.system("rm fusesoc.conf")
+os.system("rm -rf build")
